fd23cf73cfdffa7c7cc5f6672593d4446bd65f4e
ttak0422 <ttak0422@gmail.com>
2024-12-30 22:35:46 +0900

    fixup

--- denops/neorg/complete.ts
+++ denops/neorg/complete.ts
@@ -9,8 +9,7 @@ const makeSimpleStaticCompletion: (opt: {
 }) => (ctx: Context) => Promise<Item<UserData>[]> = (opt) =>
   (() => {
     return (ctx) => {
-      const matches = opt.pattern.exec(ctx.input);
-      if (matches === null) {
+      if (!opt.pattern.test(ctx.input)) {
         return Promise.resolve([]);
       }
       return Promise.resolve(opt.candinates.map((c) => ({ word: c })));
@@ -42,24 +41,12 @@ export const getMediaTypes: (ctx: Context) => Promise<Item<UserData>[]> =
 // get languages if available.
 export const getLanguages: (ctx: Context) => Promise<Item<UserData>[]> =
   (() => {
-    const pattern = /^\s*@code\s(\w)*/;
+    const pattern = /^\s*@code\s\w*$/;
     return async (ctx) => {
-      const matches = pattern.exec(ctx.input);
-      if (matches === null) {
+      if (!pattern.test(ctx.input)) {
         return [];
       }
-      const input = matches[1];
       const candinates = await getLanguageList(ctx);
-
-      return candinates.map((c) => {
-        if (c.indexOf(input) < 0) {
-          return undefined;
-        }
-        const item: Item<UserData> = {
-          word: c,
-        };
-        return item;
-      })
-        .filter((c) => c !== undefined);
+      return candinates.map((c) => ({ word: c }));
     };
   })();
