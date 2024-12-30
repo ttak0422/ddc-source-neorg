2548cd1a764a28196070bc771c58a520514124e9
ttak0422 <ttak0422@gmail.com>
2024-12-30 20:31:30 +0900

    fix(complete): remove redundant values

--- denops/neorg/complete.ts
+++ denops/neorg/complete.ts
@@ -1,5 +1,5 @@
 import { Context } from "./types.ts";
-import { CompletionItem, UserData } from "./deps/lsp.ts";
+import { UserData } from "./deps/lsp.ts";
 import { getLanguageList } from "./bindings.ts";
 import { Item } from "jsr:@shougo/ddc-vim@~9.1.0/types";
 
@@ -20,25 +20,8 @@ export const getLanguages: (ctx: Context) => Promise<Item<UserData>[]> =
         if (index < 0) {
           return undefined;
         }
-        const lspItem: CompletionItem = {
-          label: c,
-          textEdit: {
-            newText: c,
-            range: {
-              start: {
-                line: ctx.lineNr,
-                character: ctx.completePos,
-              },
-              end: {
-                line: ctx.lineNr,
-                character: ctx.completePos + c.length,
-              },
-            },
-          },
-        };
         const item: Item<UserData> = {
           word: c,
-          user_data: { lspitem: JSON.stringify(lspItem) },
         };
         return item;
       })
